72e15fff67312c7752b37aba75efe7e85d232d45
Maknight233 <90875289+Maknight233@users.noreply.github.com>
2025-01-25 18:19:32 -0500

    done for add dish

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -11,8 +11,8 @@ public interface DishMapper {
 
     /**
      * 根据分类id查询菜品数量
-     * @param categoryId
-     * @return
+     * @param categoryId category id
+     * @return number of dish that id = categoryId
      */
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
