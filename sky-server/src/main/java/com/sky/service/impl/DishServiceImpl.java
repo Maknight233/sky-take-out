e21cc391e5fbfec4dc8623ed25e73f284e3d6e98
Maknight233 <90875289+Maknight233@users.noreply.github.com>
2025-01-25 18:17:51 -0500

    done for add dish

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,13 +1,10 @@
 package com.sky.service.impl;
 
-import com.sky.context.BaseContext;
 import com.sky.dto.DishDTO;
 import com.sky.entity.Dish;
-import com.sky.entity.DishFlavor;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
 import com.sky.service.DishService;
-import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,9 +28,7 @@ public class DishServiceImpl implements DishService {
         BeanUtils.copyProperties(dishDTO, dish);
         dishMapper.addDish(dish);
         Long id = dish.getId();
-        dishDTO.getFlavors().forEach(flavor -> {
-            flavor.setDishId(id);
-        });
+        dishDTO.getFlavors().forEach(flavor -> flavor.setDishId(id));
         dishFlavorMapper.add(dishDTO.getFlavors());
     }
 }
