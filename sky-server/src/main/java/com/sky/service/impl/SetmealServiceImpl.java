405c66ca3505d16b6218ea727af55101c5d734ff
Maknight233 <90875289+Maknight233@users.noreply.github.com>
2025-01-29 22:06:39 -0500

    done for setmeal in admin end

--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -124,7 +124,6 @@ public class SetmealServiceImpl implements SetmealService {
         setmealMapper.update(setmeal);
         setmealDishMapper.deleteById(setmealDTO.getId());
         log.info("The setmealId is: {}", setmealDTO.getId());
-        log.info("The setmealDTO is: {}", setmealDTO);
         setmealDTO.getSetmealDishes().forEach(setmealDish -> setmealDish.setSetmealId(setmealDTO.getId()));
         setmealDishMapper.addAll(setmealDTO.getSetmealDishes());
     }
