ed578ec3622394c8774da9d750a2a9c9b2b838f4
Maknight233 <90875289+Maknight233@users.noreply.github.com>
2025-02-12 19:04:49 -0500

    done for shopping cart

--- sky-server/src/main/java/com/sky/controller/user/ShopingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopingCartController.java
@@ -6,7 +6,6 @@ import com.sky.result.Result;
 import com.sky.service.ShoppingCartService;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
